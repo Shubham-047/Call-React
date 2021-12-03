858007a1fd3643613258963c5a12bbcd3429882a
Shubham Kumar <shubham1406kumar@gmail.com>
2021-12-03 15:01:24 +0530

    completed

--- src/Components/Store/reducer.js
+++ src/Components/Store/reducer.js
@@ -1,24 +1,45 @@
-import { UPDATE_LEFT_OPTION, UPDATE_RIGHT_OPTION } from "./actionTypes"
+import { UPDATE_LEFT_OPTION, UPDATE_RIGHT_OPTION } from "./actionTypes";
 
 const initState = {
-     array1 : ["hi", "hello", "new", "Shukriya", "Whats up"],
- array2 : ["s", "h", "r", "p", "t"]
-}
+  array1: [
+    "hi",
+    "hello",
+    "bye",
+    "sales",
+    "grow",
+    "right",
+    "left",
+    "forward",
+    "backward",
+    "direction",
+  ],
+  array2: [
+    "DSA",
+    "JAVA",
+    "Python",
+    "c++",
+    "C",
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "MERN",
+    "MEAN",
+  ],
+};
 
 export const reducer = (state = initState, { type, payload }) => {
-    switch (type) {
-        case UPDATE_LEFT_OPTION:
-            return {
-                ...state,
-               
-            }
-        
-          case UPDATE_RIGHT_OPTION:
-            return {
-                ...state,
-            }
-        
-        default:
-            return state;
-    }
-}
+  switch (type) {
+    case UPDATE_LEFT_OPTION:
+      return {
+        ...state,
+      };
+
+    case UPDATE_RIGHT_OPTION:
+      return {
+        ...state,
+      };
+
+    default:
+      return state;
+  }
+};
